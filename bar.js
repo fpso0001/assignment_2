b347fadd357c503c6e1ebe3caca550fdad8a89b5
fpso0001 <89438934+fpso0001@users.noreply.github.com>
2021-10-18 16:39:50 +1100

    Add files via upload

---
+++ bar.js
@@ -0,0 +1,31 @@
+var vg_1 = "bar2.vg.json"; 
+
+vegaEmbed("#bar", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var vg_2 = "choro.vg.json"; 
+
+vegaEmbed("#choro", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var vg_3 = "choro_eu.vg.json"; 
+
+vegaEmbed("#choroeu", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var vg_4 = "drivers.vg.json"; 
+
+vegaEmbed("#drivers", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+var vg_5 = "prop_choro.vg.json"; 
+
+vegaEmbed("#propchoro", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+    
+
